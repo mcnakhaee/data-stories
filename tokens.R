46a0c00b6c164bc576e1e9abacefcbc8049c5e0f
mcnakhaee <mcnakhaee@gmail.org>
2020-08-01 17:29:47 -0700

    update spotify files

--- tokens.R
+++
@@ -1,6 +0,0 @@
-token <- create_token(
-  app = "my_twitter_research_app",
-  consumer_key = 'RgivABxfsX9ubdhxLxZBBh5m7',
-  consumer_secret = '5TigMInpBwJLNnFed8x8JlfNpvjMU0nOd9u1UPgRA2KOYIsNVU',
-  access_token = '496163705-nylSebOzfa5bThbRQHmS40f3eLV0X2z6V514uxcB',
-  access_secret = 'OkTZpcW9CJPMuApCQvWpZAlKR38vz8d2g9UStFDX32U3k')
